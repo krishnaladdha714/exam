7e06427dd4452e0a1b2ec400ec2fd4af9e14fc86
krishnaladdha714 <134532502+krishnaladdha714@users.noreply.github.com>
2023-05-25 01:40:57 +0530

    _3str.java

--- _3str.java
+++ _3str.java
@@ -3,22 +3,28 @@ import java.util.Scanner;
 //3.1Java Program to Reverse a String.
 //3.2) Write a Java program to check that String is palindrome or not.
 public class _3str {
-    public static void reverse(String st) {// 3.1
-        StringBuilder SB = new StringBuilder(st);
-        System.out.println(SB.reverse());
-    }
-
-    public static void checkpallindrome(String st) {// 3.2
-        StringBuilder SB = new StringBuilder(st);
-        System.out.println(st.equals(SB.reverse().toString()));
-    }
-
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nEnter String");
-        String word = new String(sc.next());
-        reverse(word);
-        checkpallindrome(word);
+        System.out.println("Enter string : ");
+        String s = sc.nextLine();
+
+        String ok = "";
+        int sz = s.length();
+
+        for (int i = sz - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            ok = ok + ch;
+        }
+        System.out.println("Reversed String is : " + ok);
+
+        if (ok.equals(s))
+
+        {
+            System.out.println("palindrone");
+        } else {
+            System.out.println(" not ");
+        }
 
     }
 }
